1b18372a07758735991052ddf26206ad0e6be15e
GpMidi <paul@gpmidi.net>
2011-02-26 15:50:43 -0500

    - Added urls

---
+++ src/BigBrotherFrontend/bigbrother/urls.py
@@ -0,0 +1,14 @@
+'''
+Created on Feb 26, 2011
+
+@author: gpmidi
+'''
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('BigBrotherFrontend.bigbrother.views',
+    
+)
